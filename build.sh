c88f34db71f212abc8880977d070b80666a0c949
huy12asd <legiahuy124578@gmail.com>
2025-10-24 23:59:39 +0700

    up3

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 # exit on error
 set -o errexit
 
-# Cài đặt các gói cần thiết của hệ thống
-apt-get update
-apt-get install -y curl gnupg
+# Thêm "sudo" vào trước các lệnh hệ thống
+sudo apt-get update
+sudo apt-get install -y curl gnupg
 
-# Thêm khoá Microsoft GPG
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+# Thêm khoá Microsoft GPG với quyền sudo
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 
-# Thêm repository của Microsoft
-curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+# Thêm repository của Microsoft (dùng tee để ghi file với quyền sudo)
+curl https://packages.microsoft.com/config/debian/11/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
 
-# Cập nhật lại danh sách gói và cài đặt driver
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
+# Cập nhật và cài đặt driver với sudo
+sudo apt-get update
+ACCEPT_EULA=Y sudo apt-get install -y msodbcsql17 unixodbc-dev
 
-# Cài đặt các thư viện Python
+# Cài đặt các thư viện Python KHÔNG cần sudo
 pip install -r requirements.txt
